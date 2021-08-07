49b312ea3662c2f8cae97abb5bb3bab09a71ce47
hj2 <2hjinyj@gmail.com>
2021-08-08 03:52:32 +0900

    first

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>오이~</Text>
       <StatusBar style="auto" />
     </View>
   );

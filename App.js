fceeb4fc6f7271b03b42a5d91d9c716ce04c4cac
hj2 <2eehjin@gmail.com>
2021-08-08 05:35:39 +0900

    user change

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>오이~</Text>
+      <Text>오이</Text>
       <StatusBar style="auto" />
     </View>
   );
